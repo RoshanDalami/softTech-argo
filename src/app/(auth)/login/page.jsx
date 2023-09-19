a076078eb7078cfc6376e741b40f240ebc745f10
RoshanDalami <roshandalami0@gmail.com>
2023-09-19 17:03:38 +0545

    navbar, login, signup page added

---
+++ src/app/(auth)/login/page.jsx
@@ -0,0 +1,9 @@
+import LoginForm from '@/Components/Login'
+
+export default function Login(){
+    return(
+        <main className='flex items-center justify-center min-h-screen ' >
+            <LoginForm/>
+        </main>
+    )
+}
